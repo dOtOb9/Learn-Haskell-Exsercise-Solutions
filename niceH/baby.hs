dc7fb3bfa73af70fa25a4ff68c94455b196142b3
dOtOb9 <dotob9@gmail.com>
2025-09-21 12:32:32 +0900

    6章まで読み終えた

--- niceH/baby.hs
+++ niceH/baby.hs
@@ -1,5 +1,33 @@
-import Distribution.Simple.Utils (xargs)
-import Control.Arrow (ArrowApply(app))
+import Data.List
+import Data.Char
+import qualified Data.Map as Map
+
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40)[1..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]
+
+encode :: Int -> String -> String
+encode offset mag = map (\c -> chr $ ord c + offset) mag
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+
+wordNum :: String -> [(String, Int)]
+wordNum = map (\ws -> (head ws, length ws)) . group . sort . words
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
 doubleMe x = x*2
 
 doubleUs x y = x*2 + y*2
@@ -188,4 +216,27 @@ maximum' = foldl1 max
 
 reverse'' :: [a] -> [a]
 reverse'' = foldl (\acc x -> x : acc) []
+
+
+phoneBook :: Map.Map String String
+phoneBook = Map.fromList $
+    [("betty","555-2938")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ]
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key xs = foldr
+                    (\(k,v) acc -> if key == k then Just v else acc)
+                    Nothing xs
+
+string2digits :: String -> [Int]
+string2digits = map digitToInt . filter isDigit
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith add xs
+    where add number1 number2 = number1 ++ ", " ++ number2
 
